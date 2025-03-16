54af0f403f82727f717d2bbd70501bbcff66a79d
Jin iPad <jinpengwu68@gmail.com>
2025-03-16 05:17:25 +0000

    update

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 console.log('aaa')
 
+
+let http = require('http')
+
+http.createServer(function (req, res){
+  res.writeHead(200, {'Content-Type':'text/html'});
+  res.end('hello world');
+
+}).listen(8080);
+
